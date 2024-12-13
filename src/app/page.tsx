9da759b78274e6e28d85b2064806b11ce7d0c2bc
lucilajuliana.jsx <lucilapastore@gmail.com>
2024-12-13 18:37:09 -0300

    feat: nft metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
+"use client"
 import FeaturesSection from '@/components/features-section';
 import HeroSection from '@/components/hero-section';
 import InfoSection from '@/components/info-section';
 import Navbar from '@/components/navbar';
-import { ThirdwebProvider } from "thirdweb/react";
+import { getContract } from 'thirdweb';
+import { sepolia } from 'thirdweb/chains';
+import { getContractMetadata } from 'thirdweb/extensions/common';
+import { ThirdwebProvider, useReadContract } from "thirdweb/react";
+import { client } from './client';
 
 export default function NFTLandingPage() {
+  const contract = getContract({
+    client: client,
+    chain: sepolia,
+    address: process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS as string
+  })
+
+  const {data: contractMetadata}=useReadContract(
+    getContractMetadata, {
+      contract: contract
+    }
+  );
+  console.log(contractMetadata);
   return (
     <ThirdwebProvider>
     <div className="min-h-screen bg-background">
@@ -18,6 +35,7 @@ export default function NFTLandingPage() {
     </ThirdwebProvider>
   )
 }
+
 
 
 
